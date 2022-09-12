37c5d0c36591c9d94e131d77ecfc823a5af5e513
Jérémy Saudemont <jeremysaudemont@gmail.com>
2022-09-12 21:04:43 +0200

    First working atempte

--- main.py
+++ main.py
@@ -1,16 +1,54 @@
-# This is a sample Python script.
+import math
+from tkinter import *
 
-# Press Maj+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+window = Tk()
+window.title('Fractal Tree')
+canvas_size = 1200
+
+canvas = Canvas(window, width=canvas_size, height=canvas_size)
+canvas.pack()
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+class Branch:
+    to_draw = []
+    length_ratio = 0.7
+    min_length = 1
+
+    def __init__(self, x, y, length, angle):
+        self.x = x
+        self.y = y
+        self.length = length
+        self.angle = angle
+
+        self.end_x = x + length * math.cos(angle)
+        self.end_y = y + length * math.sin(angle)
+
+    @classmethod
+    def draw_all(cls):
+        to_draw = cls.to_draw
+        cls.to_draw = []
+        for branch in to_draw:
+            branch.draw()
+
+        for branch in to_draw:
+            branch.grow()
+
+        if cls.to_draw:
+            cls.draw_all()
+
+    def grow(self):
+        if self.length <= self.min_length:
+            return
+        self.to_draw.append(Branch(self.end_x, self.end_y, self.length * self.length_ratio, self.angle + math.pi / 6))
+        self.to_draw.append(Branch(self.end_x, self.end_y, self.length * self.length_ratio, self.angle - math.pi / 6))
+
+    def draw(self):
+        canvas.create_line(self.x, self.y, self.end_x, self.end_y)
+        window.update()
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+Branch.to_draw.append(Branch(canvas_size/2, canvas_size, 300, -math.pi / 2))
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+Branch.draw_all()
+
+window.mainloop()
